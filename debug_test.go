03e90eb406db00e3da1c1d4d250a71177d7c00c7
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2025-12-22 10:08:13 +0100

    PMM-14577 Trigger.

--- debug_test.go
+++ debug_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/go-openapi/testify/v2/assert"
 )
 
-var logMutex = &sync.Mutex{} //nolint:gochecknoglobals
+var logMutex = &sync.Mutex{}
 
 func TestDebug(t *testing.T) {
 	// usetesting linter disabled until https://github.com/golang/go/issues/71544 is fixed for windows
@@ -24,6 +24,7 @@ func TestDebug(t *testing.T) {
 		_ = os.Remove(tmpName)
 	}()
 
+	// trigger for CI
 	// mutex for -race
 	logMutex.Lock()
 	Debug = true
